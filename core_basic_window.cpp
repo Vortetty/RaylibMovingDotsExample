5b29cc3b3971b2023c6fffcda52f88f53300a601
vortetty <vortetty@gmail.com>
2021-03-20 16:58:08 -0700

    Update0-win32-seh-rt_v6-rev0\mingw64\bin\g++.exe;C:\Program

--- core_basic_window.cpp
+++ core_basic_window.cpp
@@ -132,76 +132,131 @@ int main(int argc, char* argv[])
     //--------------------------------------------------------------------------------------
     int screenWidth = 800;
     int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Please wait, Initializing.", 10, 10, 20, LIME);
+    EndDrawing();
+
     int currentBalls = 250;
     int targetBalls = 250;
     double timeToDraw = 0;
     double tempTime = GetTime();
+    int lastscroll = 0;
+    int scrollMul = 1;
+    int targetfps = 0;
     std::vector<dot> balls;
 
 
     for (int i = 0; i < 250; i++) balls.push_back(dot(screenWidth, screenHeight));
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    //--------------------------------------------------------------------------------------
+    
+    SetTargetFPS(60);
+
+    while (lastscroll == 0){
+        BeginDrawing();
+
+        lastscroll = GetMouseWheelMove();
+        ClearBackground(RAYWHITE);
+        DrawText("Please scroll up to calibrate scrolling", 10, 10, 20, LIME);
+
+        EndDrawing();
+    }
+
+    scrollMul = 5/lastscroll;
+    
+    SetTargetFPS(1);
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Scrolling calibrated, starting.", 10, 10, 20, LIME);
+    EndDrawing();
 
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
+    targetfps = 60;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key and exit if pressed
     {
-        tempTime = GetTime();
-        targetBalls = std::max(1.0f, targetBalls - GetMouseWheelMove());
-
-        while (currentBalls != targetBalls){
-            if (currentBalls > targetBalls){
-                balls.pop_back();
-                currentBalls--;
+        if (IsWindowFocused()){
+            SetTargetFPS(targetfps);
+            if(GetKeyPressed() == KEY_M){
+                if (targetfps == 0){
+                    targetfps = 60;
+                    SetTargetFPS(60);
+                }
+                else {
+                    targetfps = 0;
+                    SetTargetFPS(0);
+                }
             }
-            else {
-                balls.push_back(dot(screenWidth, screenHeight));
-                currentBalls++;
+            
+
+            tempTime = GetTime();
+            targetBalls = std::max(1.0f, targetBalls + GetMouseWheelMove() * scrollMul);
+
+            while (currentBalls != targetBalls){
+                if (currentBalls > targetBalls){
+                    balls.pop_back();
+                    currentBalls--;
+                }
+                else {
+                    balls.push_back(dot(screenWidth, screenHeight));
+                    currentBalls++;
+                }
             }
-        }
 
-        // Update
-        for (auto& i : balls) { 
-            i.move(screenWidth, screenHeight);
-        } 
+            // Update
+            for (auto& i : balls) { 
+                i.move(screenWidth, screenHeight);
+            } 
 
-        std::vector<line> lines;
+            std::vector<line> lines;
 
-        for (auto& b1 : balls) { 
-            for (auto& b2 : balls) { 
-                if (distance(b1.pos.x, b1.pos.y, b2.pos.x, b2.pos.y) < 50) {
-                    line e = {b1.pos, b2.pos};
-                    if (std::find_if(lines.begin(), lines.end(), [&](line& s) { return line_equal(s, e); }) == lines.end()){
-                        lines.push_back(
-                            {
-                                b1.pos, b2.pos
-                            }
-                        );
+            for (auto& b1 : balls) { 
+                for (auto& b2 : balls) { 
+                    if (distance(b1.pos.x, b1.pos.y, b2.pos.x, b2.pos.y) < 50) {
+                        line e = {b1.pos, b2.pos};
+                        if (std::find_if(lines.begin(), lines.end(), [&](line& s) { return line_equal(s, e); }) == lines.end()){
+                            lines.push_back(
+                                {
+                                    b1.pos, b2.pos
+                                }
+                            );
+                        }
                     }
                 }
             }
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            for (auto& i : balls) { 
+                DrawCircle(i.pos.x, i.pos.y, 2, BLACK);
+            } 
+
+            for (auto& i : lines) { 
+                DrawLineEx(i.v1, i.v2, 3, {0, 0, 0, (unsigned char)(255-distance(i.v1.x, i.v1.y, i.v2.x, i.v2.y)/50*255)});
+            } 
+
+            DrawText(TextFormat("%2i FPS | %2i\n%2i Balls", GetFPS(), (int)(1/timeToDraw), targetBalls), 10, 10, 20, LIME);
+            DrawText(targetfps == 0 ? "Fps uncapped\nScroll to change ball count, press ESC to quit" : "Fps capped\nScroll to change ball count, press ESC to quit", 10, screenHeight-55, 20, LIME);
+            timeToDraw = GetTime() - tempTime;
+            EndDrawing();
+            //----------------------------------------------------------------------------------
         }
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        for (auto& i : balls) { 
-            DrawCircle(i.pos.x, i.pos.y, 2, BLACK);
-        } 
-
-        for (auto& i : lines) { 
-            DrawLineEx(i.v1, i.v2, 3, {0, 0, 0, (unsigned char)(255-distance(i.v1.x, i.v1.y, i.v2.x, i.v2.y)/50*255)});
-        } 
-
-        DrawText(TextFormat("%2i FPS | %2f\n%2i Balls", GetFPS(), (float)(1/timeToDraw), targetBalls), 10, 10, 20, LIME);
-        timeToDraw = GetTime() - tempTime;
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+        else {
+            SetTargetFPS(1);
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText(TextFormat("%2i FPS\nPaused", GetFPS()), 10, 10, 20, LIME);
+            EndDrawing();
+        }
     }
 
     // De-Initialization
