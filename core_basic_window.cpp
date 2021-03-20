5756073b930ed43c825f5336986b0ea9cbdf39ab
Vortetty <vortetty@gmail.com>
2021-03-20 16:01:23 -0700

    upload original

---
+++ core_basic_window.cpp
@@ -0,0 +1,213 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include <random>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+bool inRange(float low, float high, float x)
+{
+    return (low < x && x < high);
+}
+
+std::mt19937 generator;
+
+int randint(int min, int max){
+    std::uniform_int_distribution<int> distribution(min, max);
+    return distribution(generator);
+}
+
+float randFloat(float a, float b) {
+    float random = ((float) rand()) / (float) RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
+
+class dot{
+    public:
+        Vector2 vel, pos, start;
+
+        dot(int screenWidth, int screenHeight){
+            switch(randint(0, 4)){
+                case 0: // Starts on bottom
+                    pos.x = randFloat(0+50, screenWidth-50);
+                    pos.y = 0;
+                    vel.x = randFloat(-1.5, 1.5); //Random -1 to 1 velocity on x
+                    vel.y = randFloat(1, 1.5); //Random -1 to 1 velocity on y
+                    if (vel.x == 0) vel.x = 1;
+                    if (vel.y == 0) vel.y = 1;
+                    break;
+                case 1: // Starts on top
+                    pos.x = randFloat(0+50, screenWidth-50);
+                    pos.y = screenHeight-1;
+                    vel.x = randFloat(-1.5, 1.5); //Random -1 to 1 velocity on x
+                    vel.y = -randFloat(1, 1.5); //Random -1 to 1 velocity on y
+                    if (vel.x == 0) vel.x = -1;
+                    if (vel.y == 0) vel.y = -1;
+                    break;
+                case 2: // Starts on left
+                    pos.x = 0;
+                    pos.y = randFloat(0+50, screenHeight-50);
+                    vel.x = randFloat(1, 1.5); //Random -1 to 1 velocity on x
+                    vel.y = randFloat(-1.5, 1.5); //Random -1 to 1 velocity on y
+                    if (vel.x == 0) vel.x = 1;
+                    if (vel.y == 0) vel.y = 1;
+                    break;
+                case 3: // Starts on right
+                    pos.x = screenWidth-1;
+                    pos.y = randFloat(0+50, screenHeight-50);
+                    vel.x = -randFloat(1, 1.5); //Random -1 to 1 velocity on x
+                    vel.y = randFloat(-1.5, 1.5); //Random -1 to 1 velocity on y
+                    if (vel.x == 0) vel.x = -1;
+                    if (vel.y == 0) vel.y = -1;
+                    break;
+            };
+
+            start.x = pos.x;
+            start.y = pos.y;
+        };
+
+        dot(float vx, float vy, int sx, int sy){
+            vel.x = vx;
+            vel.y = vy;
+            pos.x = sx;
+            pos.y = sy;
+            start.x = pos.x;
+            start.y = pos.y;
+        };
+
+        void move(int screenWidth, int screenHeight){
+            pos.x = pos.x + vel.x;
+            pos.y = pos.y + vel.y;
+
+            if (!inRange(0, screenWidth, pos.x) || !inRange(0, screenHeight, pos.y)) {
+                pos.x = start.x;
+                pos.y = start.y;
+            }
+        }
+};
+
+float distance(int x1, int y1, int x2, int y2)
+{
+    // Calculating distance
+    return sqrt(pow(x2 - x1, 2) + 
+                pow(y2 - y1, 2) * 1.0);
+}
+
+struct line {
+    Vector2 v1, v2;
+};
+
+bool vec_equal(Vector2 &a, Vector2 &b) {
+    return a.x==b.x&&a.y==b.y;
+}
+bool line_equal(line &a, line &b) {
+    return vec_equal(a.v1, b.v1)&&vec_equal(a.v2, b.v2);
+}
+
+int main(int argc, char* argv[])
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+    int currentBalls = 250;
+    int targetBalls = 250;
+    double timeToDraw = 0;
+    double tempTime = GetTime();
+    std::vector<dot> balls;
+
+
+    for (int i = 0; i < 250; i++) balls.push_back(dot(screenWidth, screenHeight));
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key and exit if pressed
+    {
+        tempTime = GetTime();
+        targetBalls = std::max(1.0f, targetBalls - GetMouseWheelMove());
+
+        while (currentBalls != targetBalls){
+            if (currentBalls > targetBalls){
+                balls.pop_back();
+                currentBalls--;
+            }
+            else {
+                balls.push_back(dot(screenWidth, screenHeight));
+                currentBalls++;
+            }
+        }
+
+        // Update
+        for (auto& i : balls) { 
+            i.move(screenWidth, screenHeight);
+        } 
+
+        std::vector<line> lines;
+
+        for (auto& b1 : balls) { 
+            for (auto& b2 : balls) { 
+                if (distance(b1.pos.x, b1.pos.y, b2.pos.x, b2.pos.y) < 50) {
+                    line e = {b1.pos, b2.pos};
+                    if (std::find_if(lines.begin(), lines.end(), [&](line& s) { return line_equal(s, e); }) == lines.end()){
+                        lines.push_back(
+                            {
+                                b1.pos, b2.pos
+                            }
+                        );
+                    }
+                }
+            }
+        }
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        for (auto& i : balls) { 
+            DrawCircle(i.pos.x, i.pos.y, 2, BLACK);
+        } 
+
+        for (auto& i : lines) { 
+            DrawLineEx(i.v1, i.v2, 3, {0, 0, 0, (unsigned char)(255-distance(i.v1.x, i.v1.y, i.v2.x, i.v2.y)/50*255)});
+        } 
+
+        DrawText(TextFormat("%2i FPS | %2f\n%2i Balls", GetFPS(), (float)(1/timeToDraw), targetBalls), 10, 10, 20, LIME);
+        timeToDraw = GetTime() - tempTime;
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------   
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
---
+++ make.bat
@@ -0,0 +1,45 @@
+::@echo off
+:: .
+:: Compile your examples using:  raylib_compile_execute.bat core/core_basic_window.c
+:: .
+:: > Setup required Environment
+:: -------------------------------------
+set RAYLIB_SRC="raylib\src"
+set RAYLIB_RES_FILE="raylib\src\raylib.rc.data"
+set COMPILER="TDM-GCC\bin\g++.exe"
+set MSVC="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx86\x86\cl.exe"
+set ORIG_PATH=%PATH%
+set TDM_LIBS="TDM-GCC\x86_64-w64-mingw32\lib"
+
+:: Get full filename path for input file %1
+set FILENAME=%~f1
+set NAMEPART=%FILENAME:~0,-4%
+cd %~dp0
+
+:: .
+:: > Cleaning latest build
+:: ---------------------------
+cmd /c if exist %NAMEPART%.exe del /F %NAMEPART%.exe
+
+:: .
+:: > Compiling program
+:: --------------------------
+:: -s        : Remove all symbol table and relocation information from the executable
+:: -O2       : Optimization Level 2, this option increases both compilation time and the performance of the generated code
+:: -std=c99  : Use C99 language standard
+:: -Wall     : Enable all compilation Warnings
+:: -mwindows : Compile a Windows executable, no cmd window
+%COMPILER% %FILENAME% %RAYLIB_RES_FILE% -o %NAMEPART%.x86.exe -s -Ofast -I%RAYLIB_SRC% -L%RAYLIB_SRC% -Lextra-dlls -lraylib -lopengl32 -lgdi32 -lwinmm -std=c++17 -Wall -mwindows -m32 -static
+
+:: Max optimize with upx
+upx\upx.exe --ultra-brute -9 --best -v -f --compress-icons=1 --compress-resources=1 --strip-relocs=1 --compress-exports=1 %NAMEPART%.x86.exe
+
+
+:: Ensure dirs for built exes
+mkdir bin\x86
+
+:: Move builds to respective binary directories
+move %NAMEPART%.x86.exe bin\x86
+
+:: Clean up
+set PATH=%ORIG_PATH%
